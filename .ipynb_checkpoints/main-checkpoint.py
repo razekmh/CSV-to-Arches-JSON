3dd579d0b6ff707f2772e4ba32622ccf2ba3cbb3
Mahmoud <razekmh@gmail.com>
2021-08-31 22:06:35 +0100

    base cascade

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,129 @@
+import csv
+import json
+import uuid
+# import yaml
+
+# TODO
+# multiple instances per resource:
+#   group the rows according to the resourceID
+#   create multiple tiles per attribute
+# cascading stracture:
+#   check for parent tiles and create them >> unchecked
+#
+
+
+# parameters
+res_model_json = "Activity Resource Model.json"
+data_csv = "Activity Resource Model_data.csv"
+
+res_model_json = "Test JSON upload.json"
+data_csv = "test_json_upload_data.csv"
+
+# read json from resource model structure
+with open(res_model_json, 'r') as file_rm:
+    rm_json = json.load(file_rm)
+
+# copy nodegroup object
+nodegroup_list = rm_json['graph'][0]['nodegroups']
+# print(nodegroup_list)
+# get graphid from the first card
+graphid = rm_json['graph'][0]['cards'][0]['graph_id']
+
+# get node attributes
+node_list = []
+for node in rm_json['graph'][0]['nodes']:
+    node_list.append({
+        'nodeid' : node['nodeid'],
+        'nodegroup_id' : node['nodegroup_id'],
+        'name' : node['name'],
+    })
+
+# gruop nodes based on the nodegroup_id
+grouped_nodes = {}
+for node in node_list:
+        nodegroup_id = node["nodegroup_id"]
+        if nodegroup_id in grouped_nodes:
+                grouped_nodes[nodegroup_id].append(node)
+        else:
+                grouped_nodes[nodegroup_id] = [node]
+    
+
+def create_res(graphid, grouped_nodes, row, legacy_id=''):
+    # create resource instance
+    id = legacy_id
+    if id == '':
+        id = str(uuid.uuid4())
+    res = {}
+    res['resourceinstance'] = {
+        "resourceinstanceid" : id, "graph_id" : str(graphid), "legacyid" : id}
+    tiles =[]
+    for item in grouped_nodes.items():
+        if item[0]:
+            tiles.extend(create_tile(item, id, row))
+    res['tiles'] = tiles
+    return res
+
+
+def create_tile(card, resid, row, parent=None, tiles=None):
+    # create tile
+    # print(card)
+    if not parent: 
+        tiles = []
+    tile = {}   
+    tile['tileid'] = str(uuid.uuid4())
+    tile['resourceinstance_id'] = resid
+    tile['nodegroup_id'] = card[0]
+    tile['sortorder'] = 0
+    tile['parenttile_id'] = None
+    tile['data'] = Build_data(card, row)
+    tiles.append(tile)
+    for nodegroup in nodegroup_list:
+        if tile['nodegroup_id'] == nodegroup['nodegroupid'] and nodegroup['parentnodegroup_id'] is not None:
+                print(nodegroup['parentnodegroup_id'])
+                create_tile(card=(nodegroup['parentnodegroup_id'],None), resid=resid, row=row, tiles=tiles)
+            #  and
+
+    # print(card)
+    # here check for parent and then recurseive call to the same function with the parent ID to create a parent tile
+    # create_tile(None, resid,  
+    return tiles
+
+def Build_data(card,row):
+    data = {}
+    # print(card, "/n/n/n")
+    # print(data_dict)
+    if card[1] is None:
+        print("empty card")
+        return data
+    card_data = card[1]
+    for value in card_data:
+        if value['name'] in row.keys():
+            data[value['nodeid']] = row[value['name']]
+    return data
+
+
+def create_json(resources):
+
+    json_data = {}
+    resources_list = {'resources' : resources}
+    json_data['business_data'] = resources_list
+    return json_data
+
+
+
+def build_json():
+    resources = []
+    with open(data_csv) as file_act_data:
+        act_data = csv.DictReader(file_act_data)
+        for row in act_data:
+            resources.append(create_res(graphid, grouped_nodes, row))
+    
+    built = create_json(resources)
+    # print(built)
+    with open(f'{data_csv[:-4]}.json', 'w') as fp:
+        json.dump(built, fp)
+    # return built
+
+build_json()
+
+print("Done!")
--- utils_test.py
+++ utils_test.py
@@ -26,7 +26,7 @@ node_list = []
 for node in rm_json['graph'][0]['nodes']:
     node_list.append({
         'nodeid' : node['nodeid'],
-        'nodegroup_id' : node['nodegroup_id'],
+        'nodegroupid' : node['nodegroup_id'],
         'name' : node['name'],
         'istopnode' : node['istopnode'],
     })
@@ -35,23 +35,90 @@ base_structure = {}
 for node in node_list:
     # print(node)
     for group in nodegroup_list:
-        if node['nodegroup_id'] == group['nodegroupid']:
+        if node['nodegroupid'] == group['nodegroupid']:
             pass
             # print(f"nodeid = {node['nodeid']}, groupid = {node['nodegroup_id']}, parentgroup = {group['parentnodegroup_id']}")
 
 node_inventory = node_list.copy()
+nodegroup_inventory = nodegroup_list.copy()
+node_inventory.remove([node for node in node_inventory if node['istopnode']][0])
+# print(nodegroup_inventory)
 
-while len(node_inventory) > 0:
-    for node in node_list:
-        base_structure = defaultdict(list)
-        # print(node['istopnode'])
-        if node['istopnode']:
-            base_structure["nodegroup_id"] = node['nodegroup_id']
-            for check_node in node_list: 
-                base_structure["children_groups"].extend([group['nodegroupid'] for group in nodegroup_list if group['parentnodegroup_id'] == check_node['nodegroup_id']])
-            # base_structure["children_nodes"]
-            # print(node)
-            if node in node_inventory:
-                node_inventory.remove(node)
+# topnode has no nodegroupid  
+# 
 
-    print(base_structure)    
+base_structure = defaultdict(list)
+for main_node in node_list:
+    if main_node['istopnode']:
+        base_structure["nodegroupid"] = main_node['nodeid']
+        break
+    # print(base_structure)
+    # break
+    group_structure = []
+for nodegroup in nodegroup_list:
+    if nodegroup['parentnodegroup_id']:
+        pass
+    else:
+        group_structure.append({
+            'nodegroupid' : nodegroup['nodegroupid'],
+            'parentgroup' : None,
+            "childrengroups" : [],
+            "childrennodes" : [],
+            })
+        nodegroup_inventory.remove(nodegroup)
+
+print(group_structure)
+
+def check_group(group_structure,checkgroup):
+    insertion_detected = False
+    # place a group in its location within the structure
+    for nodegroup in group_structure:
+        if checkgroup['parentnodegroup_id'] == nodegroup['nodegroupid']:
+            nodegroup['childrengroups'].append({
+            'nodegroupid' : checkgroup['nodegroupid'],
+            'parentgroup' : None,
+            "childrengroups" : [],
+            "childrennodes" : [],
+            })
+            insertion_detected = True
+            break
+
+        if not insertion_detected: 
+            nodegroup['childrengroups'], insertion_detected = check_group(nodegroup['childrengroups'],checkgroup)
+            print(nodegroup,"\n\n")
+    return (group_structure, insertion_detected)
+
+while len(nodegroup_inventory) > 0:
+    for nodegroup in nodegroup_inventory:
+        group_structure, insertion_detected = check_group(group_structure, nodegroup)
+        if insertion_detected: 
+            nodegroup_inventory.remove(nodegroup)
+
+    
+
+
+print(len(group_structure))
+print(group_structure)
+print(len(node_inventory))
+        
+
+    
+
+        # check if the parnet exists 
+
+
+# while len(node_inventory) > 0:
+#     for node in node_list:
+#         base_structure = defaultdict(list)
+#         # print(node['istopnode'])
+#         if node['istopnode']:
+#             base_structure["nodegroup_id"] = node['nodegroup_id']
+#             for check_node in node_list: 
+#                 # base_structure["children_groups"].extend([group['nodegroupid'] for group in nodegroup_list if group['parentnodegroup_id'] == check_node['nodegroup_id']])
+#                 print([group['nodegroupid'] for group in nodegroup_list if group['parentnodegroup_id'] == check_node['nodegroup_id']])
+#             # base_structure["children_nodes"]
+#             # print(node)
+#             if node in node_inventory:
+#                 node_inventory.remove(node)
+
+    # print(base_structure)    
